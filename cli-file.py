cd29ba84aa4148cf23e3354599af2f38d8329c60
Titor <gj94gj94ao4@gmail.com>
2023-06-14 11:38:30 +0000

    add cli tool

---
+++ cli-file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import fire
+from mylib import core
+
+if __name__ == '__main__':
+    fire.Fire(core)
--- mylib/core.py
+++ mylib/core.py
@@ -7,3 +7,9 @@ def wiki(name='Python (programming language)', length=1):
     """This function returns a summary of a wikipedia article."""
 
     return wikipedia.summary(name, length)
+
+
+def search(name='Python (programming language)'):
+    """This function search wikipedia for a given string."""
+
+    return wikipedia.search(name)
